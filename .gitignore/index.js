023eb0585b0a747e5fc2745d1e42a81109b39f52
s944939 <35062870+s944939@users.noreply.github.com>
2018-01-03 18:07:48 +0800

    Add files via upload

---
+++ .gitignore/index.js
@@ -0,0 +1,9 @@
+var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+  channelId: 1527980144,
+  channelSecret: db006a4350f4022498aff3569230102c
+,
+  channelAccessToken: 15SsVna4LTWFULOWPNxrE+eBMnnr13ryzbtMwxbhExmcZv01qmFohApIDdRLUbmoIebsTvBdU5WcUM+pTvZRGMsESzzEqmWU1JZEgKUDkDjGMsOHdkRyfUqUcsY82brYrqdIEK9/sS763UPwKH4z2gdB04t89/1O/w1cDnyilFU=
+});
